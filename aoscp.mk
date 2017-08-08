73c5b7fdef25749562cd77f23ae7e9df25bca5da
thp1997 <tirthpatel49@gmail.com>
2017-08-08 09:51:08 +0000

    AOSCP BringUP.

--- aoscp.mk
+++ aoscp.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := aoscp_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
